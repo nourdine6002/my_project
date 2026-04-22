e72dcd37540e9e63880306bb723acabde42aefe7
Your Name <your_email@example.com>
2026-04-22 13:15:27 +0100

    edit

--- up/42-helper/js/config.js
+++ up/42-helper/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  OPENROUTER_KEY: "csk-ct4rxyrfxw8whytrnn32en928temdvhkpvjhwfdnvp6wkjdp",
+  OPENROUTER_KEY: "csk-f4h3hkkmpthdpp3ct2p5mrcnpnedptwmyyhw2ehtneeehh6d",
   AI_MODEL: "llama3.1-8b",
   AI_BASE_URL: "https://api.cerebras.ai/v1/chat/completions"
 };
